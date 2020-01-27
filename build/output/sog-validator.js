f7b187909d3e23c7eeb95139ad78cef6cf6a940a
Bob Oleksii <bob.oleksii@messeprojekt.de>
2020-01-27 19:07:42 +0200

    Refactoring

--- build/output/sog-validator.js
+++ build/output/sog-validator.js
@@ -1,5 +1,5 @@
 /*
- * SOG Validator Library v2.0 revision 18e21d0
+ * SOG Validator Library v2.0 revision 444ff92
  * Copyright 2011-2020 SOG Validator Ltd. All rights reserved.
  */
 ;(function (root, factory) {
@@ -20,7 +20,7 @@ var _typeLookup = function() {
   }
   return result;
 }();
-var abv = {version:"2.0", revision:"18e21d0", config:{}, common:{}, validators:{}, registry:function(validator) {
+var abv = {version:"2.0", revision:"444ff92", config:{}, common:{}, validators:{}, registry:function(validator) {
   var __v = [validator];
   var __validator = new __v[0](null, {}, {}, "en", true);
   var alias = __validator.alias;
